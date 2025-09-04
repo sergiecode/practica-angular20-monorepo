1eb1806eee84501afda8ee9c9538e12930effb42
Sergie Code <sergiosnsdk@gmail.com>
2025-09-04 11:32:32 -0300

    package-based-npm

---
+++ packages/ui-shared/src/lib/ui-shared/ui-shared.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lib-ui-shared',
+  imports: [],
+  templateUrl: './ui-shared.html',
+  styleUrl: './ui-shared.css',
+})
+export class UiShared {}
