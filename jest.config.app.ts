aed0fba5cb1aa52a11e55f3802a1ff7e75093900
Sergie Code <sergiosnsdk@gmail.com>
2025-09-04 12:10:58 -0300

    angular-based-monorepo

---
+++ jest.config.app.ts
@@ -0,0 +1,25 @@
+export default {
+  displayName: 'software-company-angular20-monorepo',
+  preset: './jest.preset.js',
+  setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
+  coverageDirectory: './coverage/software-company-angular20-monorepo',
+  transform: {
+    '^.+\\.(ts|mjs|js|html)$': [
+      'jest-preset-angular',
+      {
+        tsconfig: '<rootDir>/tsconfig.spec.json',
+        stringifyContentPathRegex: '\\.(html|svg)$',
+      },
+    ],
+  },
+  transformIgnorePatterns: ['node_modules/(?!.*\\.mjs$)'],
+  snapshotSerializers: [
+    'jest-preset-angular/build/serializers/no-ng-attributes',
+    'jest-preset-angular/build/serializers/ng-snapshot',
+    'jest-preset-angular/build/serializers/html-comment',
+  ],
+  testMatch: [
+    '<rootDir>/src/**/__tests__/**/*.[jt]s?(x)',
+    '<rootDir>/src/**/*(*.)@(spec|test).[jt]s?(x)',
+  ],
+};
---
+++ src/ui-shared/src/lib/ui-shared/ui-shared.spec.ts
@@ -0,0 +1,21 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { UiShared } from './ui-shared';
+
+describe('UiShared', () => {
+  let component: UiShared;
+  let fixture: ComponentFixture<UiShared>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [UiShared],
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(UiShared);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
